f3edaf71b7a1c228b79cbe1308879aed98cf2bdf
ron93 <ronmkare1@gmail.com>
2017-03-08 16:11:27 +0300

    auth module auth page

--- app/auth/controllers.py
+++ app/auth/controllers.py
@@ -2,6 +2,10 @@ from flask import Flask,Blueprint,render_template
 
 module =Blueprint('auth',__name__,url_prefix='/auth')
 
+@module.route('/')
+def auth():
+    return render_template('auth/auth.html')
+
 @module.route('/signup')
 def signup():
     return render_template('auth/signup.html')
