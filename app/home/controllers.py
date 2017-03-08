88f3774c21af643389f3907407bb62869e38cbe1
ron93 <ronmkare1@gmail.com>
2017-03-08 16:34:41 +0300

    .gitignore

--- app/home/controllers.py
+++ app/home/controllers.py
@@ -8,7 +8,6 @@ module = Blueprint('home', __name__, url_prefix='/home'
 
 @module.route('/')
 def home():
-    try:
-        return render_template('home/home.html')
-    except TemplateNotFound:
-        abort(404)
+    
+    return render_template('home/home.html')
+   
